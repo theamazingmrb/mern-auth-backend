e3235f6f2b6feca9031ab4de28ab05cdef9af567
Billie Heidelberg <billie.heidelberg.jr@gmail.com>
2021-01-06 17:43:11 -0800

    quick push

--- middlewares/verifySignup.js
+++ middlewares/verifySignup.js
@@ -22,13 +22,13 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
     User.findOne({
         email: req.body.email
       }).exec((err, user) => {
+        if (user) {
+            res.status(400).send({ message: "Failed! Email is already in use!" });
+            return;
+          }
+
         if (err) {
           res.status(500).send({ message: err });
-          return;
-        }
-
-        if (user) {
-          res.status(400).send({ message: "Failed! Email is already in use!" });
           return;
         }
   
